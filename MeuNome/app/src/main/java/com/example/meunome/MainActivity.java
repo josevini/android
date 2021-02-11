844b88e40390bd6a8cb1128f038a69e305827445
José Vinícius Silva <ferreiravini468@gmail.com>
2021-02-11 20:08:11 -0300

    Update MainActivity.java

--- MeuNome/app/src/main/java/com/example/meunome/MainActivity.java
+++ MeuNome/app/src/main/java/com/example/meunome/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.meunome;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText nome;
+    TextView resposta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nome = (EditText) findViewById(R.id.editNome);
+        resposta = (TextView) findViewById(R.id.txtRes);
+    }
+
+    public void clicou(View view) {
+        String n = nome.getText().toString();
+        resposta.setText("Seja bem vindo(a), " + n + "!");
     }
 }
