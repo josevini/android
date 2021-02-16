d8ab12a9a289553e0cde52e5a27d8842cfef2746
José Vinícius Silva <ferreiravini468@gmail.com>
2021-02-16 19:18:41 -0300

    Update MainActivity.java

--- EstudonautaApp/app/src/main/java/com/example/estudonautaapp/MainActivity.java
+++ EstudonautaApp/app/src/main/java/com/example/estudonautaapp/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.estudonautaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    Intent tela;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +17,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clicouEquipe(View view) {
-
+        tela = new Intent(getApplicationContext(), Equipe.class);
+        startActivity(tela);
     }
 
     public void clicouCursos(View view) {
