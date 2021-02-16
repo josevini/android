4c5ea579da5decd88956d582e5a39eed0f896118
José Vinícius Silva <ferreiravini468@gmail.com>
2021-02-16 19:44:01 -0300

    Update Midia.java

--- EstudonautaApp/app/src/main/java/com/example/estudonautaapp/Midia.java
+++ EstudonautaApp/app/src/main/java/com/example/estudonautaapp/Midia.java
@@ -3,6 +3,7 @@ package com.example.estudonautaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Midia extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class Midia extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_midia);
+    }
+
+    public void clickVoltar(View view) {
+        finish();
     }
 }
