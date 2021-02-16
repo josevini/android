22adeebd2278833fc769147bf7bcc0e15ecab71b
José Vinícius Silva <ferreiravini468@gmail.com>
2021-02-16 19:24:50 -0300

    Update Equipe.java

--- EstudonautaApp/app/src/main/java/com/example/estudonautaapp/Equipe.java
+++ EstudonautaApp/app/src/main/java/com/example/estudonautaapp/Equipe.java
@@ -3,6 +3,7 @@ package com.example.estudonautaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Equipe extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class Equipe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_equipe);
+    }
+
+    public void clickVoltar(View view) {
+        finish();
     }
 }
