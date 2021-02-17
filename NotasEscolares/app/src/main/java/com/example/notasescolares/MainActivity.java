67def08d7ee9f4bf34796c7d5c5ccc578bae2eb0
José Vinícius Silva <ferreiravini468@gmail.com>
2021-02-17 00:19:37 -0300

    Update app

--- NotasEscolares/app/src/main/java/com/example/notasescolares/MainActivity.java
+++ NotasEscolares/app/src/main/java/com/example/notasescolares/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.notasescolares;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Intent tela;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickVerMaterias(View view) {
+        tela = new Intent(getApplicationContext(), Materias.class);
+        startActivity(tela);
     }
 }
---
+++ NotasEscolares/app/src/main/java/com/example/notasescolares/Materias.java
@@ -0,0 +1,14 @@
+package com.example.notasescolares;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Materias extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_materias);
+    }
+}
