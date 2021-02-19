d83a2d99aeed248ac880a51184518fcbe09115f8
José Vinícius Silva <ferreiravini468@gmail.com>
2021-02-19 14:02:35 -0300

    Update Cursos.java

--- EstudonautaApp/app/src/main/java/com/example/estudonautaapp/Cursos.java
+++ EstudonautaApp/app/src/main/java/com/example/estudonautaapp/Cursos.java
@@ -8,6 +8,8 @@ import android.view.View;
 
 public class Cursos extends AppCompatActivity {
 
+    Intent nav;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +18,33 @@ public class Cursos extends AppCompatActivity {
 
     public void clickVoltar(View view) {
         finish();
+    }
+
+    public void clickPortuol(View view) {
+
+    }
+
+    public void clickLingC(View view) {
+
+    }
+
+    public void clickAndroidStudio(View view) {
+
+    }
+
+    public void clickPHP(View view) {
+
+    }
+
+    public void clickCSharp(View view) {
+
+    }
+
+    public void clickKotlin(View view) {
+
+    }
+
+    public void clickVideo(View view) {
+
     }
 }
